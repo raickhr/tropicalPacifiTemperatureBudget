1f66897040c374374706eaa635b96aa89903cbd5
Shikhar <ckhr.rai@gmail.com>
2025-10-29 21:18:03 -0400

    subsetting

--- codes/getTropicalPacificData_upto330mdepth.py
+++ codes/getTropicalPacificData_upto330mdepth.py
@@ -5,27 +5,23 @@ import os
 import gc
 from datetime import datetime, timedelta
 
-startDate = datetime(2017, 6, 1)
-endDate = datetime(2020, 3, 31)
+startDate = datetime(2017, 12, 31)
+endDate = datetime(2019, 7, 1)
 curDate = startDate
 
 while curDate <= endDate:
-    writeFname = f'/proj/cdx/hseo/Data/GLORYS12v1/PCF/GLORYS12v1_dailyAvg_{curDate:%Y-%m-%d}.nc'
+    writeFname = f'/srv/seolab/srai/tropicalPacifiTemperatureBudget/WPWP_GLORYS_data/GLORYS12v1_dailyAvg_{curDate:%Y-%m-%d}.nc'
     if os.path.exists(writeFname):
         curDate += timedelta(days=1)
         continue
 
-    folder = f'/proj/cmip6/data/ocean_reanalysis/glorys12v1/{curDate.year:04d}/'
-    fname1 = f'*{curDate:%Y%m%d}*.nc'
-    fname2 = f'*{curDate:%Y-%m-%d}*.nc'
-
-    readFname1 = glob.glob(folder + fname1)
-    readFname2 = glob.glob(folder + fname2)
-
-    if readFname1:
-        readFname = readFname1[0]
-    elif readFname2:
-        readFname = readFname2[0]
+    folder = f'/srv/cdx/hseo/Data/GLORYS12v1/PCF/{curDate.year:04d}/'
+    fname = f'GLORYS12v1_dailyAvg_{curDate:%Y-%m-%d}.nc'
+    
+    readFname = glob.glob(folder + fname)
+    
+    if readFname:
+        readFname = readFname[0]
     else:
         print(f'ERROR: File not found for {curDate}')
         curDate += timedelta(days=1)
@@ -36,13 +32,14 @@ while curDate <= endDate:
         ds = xr.open_dataset(readFname)  # 🚫 No Dask!
 
         # Subset spatial domain
-        ds = ds.sel(latitude=slice(-21, 21))
+        ds = ds.sel(latitude=slice(-16, 16))
+        ds = ds.sel(depth=slice(-1, 300))
 
         # Fast longitude shift
         new_lon = (ds['longitude'].values + 360) % 360
         ds = ds.assign_coords(longitude=new_lon)
         ds = ds.sortby('longitude')
-        ds = ds.sel(longitude=slice(109, 301))
+        ds = ds.sel(longitude=slice(106, 296))
 
         # Write immediately
         ds.to_netcdf(writeFname, unlimited_dims='time')
